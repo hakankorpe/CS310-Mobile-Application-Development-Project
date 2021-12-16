8035106d101c2cf5fb353fecbd57bce82ad9bffc
sayanarman <sayanarman@sabanciuniv.edu>
2021-12-16 21:23:24 +0300

    Implemented static bookmark view with its bookmark tile
    Make tile informations dynamic later on by database connection.

--- lib/routes/bookmarks_view.dart
+++ lib/routes/bookmarks_view.dart
@@ -1,4 +1,6 @@
+import 'package:cs310_footwear_project/components/footwear_item.dart';
 import 'package:cs310_footwear_project/services/analytics.dart';
+import 'package:cs310_footwear_project/ui/bookmarks_tile.dart';
 import 'package:cs310_footwear_project/ui/navigation_bar.dart';
 import 'package:cs310_footwear_project/utils/color.dart';
 import 'package:cs310_footwear_project/utils/dimension.dart';
@@ -19,11 +21,28 @@ class BookmarksView extends StatefulWidget {
 }
 
 class _BookmarksViewState extends State<BookmarksView> {
+
+  int bookmarkCount = 0;
+
   @override
   Widget build(BuildContext context) {
     print("BookmarksView build is called.");
 
     setCurrentScreen(widget.analytics, "Bookmarks View", "bookmarksView");
+
+    const dummyImageUrl =
+        "https://media.istockphoto.com/vectors/running-shoes-line-and-glyph-icon-fitness-and-sport-gym-sign-vector-vector-id898039038?k=20&m=898039038&s=612x612&w=0&h=Qxqdsi9LAtFVNYkgjnN6GVvQ4aDaRtwyIjinns3L6j0=";
+
+    final dummyItem = FootWearItem(
+      imageUrl: dummyImageUrl,
+      brandName: "Nike",
+      sellerName: "Melinda",
+      price: 3.99,
+      rating: 3.5,
+      reviews: 1000,
+      discount: 0.25,
+      stockCount: 27,
+    );
 
     return Scaffold(
       backgroundColor: AppColors.scaffoldBackgroundColor,
@@ -36,6 +55,56 @@ class _BookmarksViewState extends State<BookmarksView> {
         ),
         centerTitle: true,
         iconTheme: kAppBarIconStyle,
+      ),
+      body: Padding(
+        padding: Dimen.regularPadding,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: bookmarkCount == 0 ? MainAxisAlignment.spaceBetween : MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              bookmarkCount == 0
+                  ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Text(
+                    "No Bookmarks!",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 22,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: Dimen.sizedBox_15,
+                  ),
+                  Text(
+                    "Don't forget to bookmark the products you like "
+                        "the most so that you can find those easily over here.",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              )
+                  : SingleChildScrollView(
+                child: Wrap(
+                  children: [
+                    const Divider(thickness: Dimen.divider_2,),
+                    BookmarksTile(product: dummyItem,),
+                    BookmarksTile(product: dummyItem,),
+                    BookmarksTile(product: dummyItem,),
+                    BookmarksTile(product: dummyItem,),
+                    BookmarksTile(product: dummyItem,),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: NavigationBar(index: 7,),
     );
---
+++ lib/ui/bookmarks_tile.dart
@@ -0,0 +1,141 @@
+import 'dart:ui';
+
+import 'package:cs310_footwear_project/components/footwear_item.dart';
+import 'package:cs310_footwear_project/utils/dimension.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class  BookmarksTile extends StatelessWidget {
+
+  final FootWearItem product;
+
+  const BookmarksTile({
+    required this.product,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: Dimen.appBarElevation,
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Image.network(
+                    product.imageUrl,
+                    width: MediaQuery.of(context).size.width /5.5,
+                    height: MediaQuery.of(context).size.width /5.5,
+                  ),
+                  Text(
+                    product.brandName,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(width: Dimen.sizedBox_15,),
+              Column(
+                //crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      const Text(
+                        "Brand Name: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        product.brandName,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: Dimen.sizedBox_5,),
+                  Row(
+                    children: [
+                      const Text(
+                        "Price: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        "${product.price.toString()}₺",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: Dimen.sizedBox_5,),
+                  Row(
+                    children: [
+                      const Text(
+                        "Rating: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      RatingBar.builder(
+                        initialRating: product.rating,
+                        minRating: 0,
+                        direction: Axis.horizontal,
+                        allowHalfRating: true,
+                        itemCount: 5,
+                        itemSize: 20,
+                        itemBuilder: (context, _) => const Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                          size: 5,
+                        ),
+                        onRatingUpdate: (rating) {
+                          print(rating);
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(width: Dimen.sizedBox_15,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    children: const [
+                      Text(
+                        "Seller:",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: Dimen.sizedBox_5,),
+                  Row(
+                    children: [
+                      Text(
+                        "${product.sellerName}",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const Divider(thickness: Dimen.divider_2,),
+        ],
+      ),
+    );
+  }
+}
