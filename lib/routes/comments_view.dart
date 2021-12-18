e2cacb12f243238146d331f97758938ef92dcfef
sayanarman <sayanarman@sabanciuniv.edu>
2021-12-18 15:49:47 +0300

    Implemented static comments view with its comments tile
    There is an overflow issue with the comments Tile. Fix it later.

--- lib/routes/comments_view.dart
+++ lib/routes/comments_view.dart
@@ -1,4 +1,6 @@
+import 'package:cs310_footwear_project/components/footwear_item.dart';
 import 'package:cs310_footwear_project/services/analytics.dart';
+import 'package:cs310_footwear_project/ui/comments_tile.dart';
 import 'package:cs310_footwear_project/ui/navigation_bar.dart';
 import 'package:cs310_footwear_project/utils/color.dart';
 import 'package:cs310_footwear_project/utils/dimension.dart';
@@ -19,8 +21,26 @@ class CommentsView extends StatefulWidget {
 }
 
 class _CommentsViewState extends State<CommentsView> {
+
+  int commentCount = 1;
+
   @override
   Widget build(BuildContext context) {
+
+    const dummyImageUrl =
+        "https://media.istockphoto.com/vectors/running-shoes-line-and-glyph-icon-fitness-and-sport-gym-sign-vector-vector-id898039038?k=20&m=898039038&s=612x612&w=0&h=Qxqdsi9LAtFVNYkgjnN6GVvQ4aDaRtwyIjinns3L6j0=";
+
+    final dummyItem = FootWearItem(
+      imageUrl: dummyImageUrl,
+      brandName: "Nike",
+      sellerName: "Melinda",
+      price: 3.99,
+      rating: 4.8,
+      reviews: 1000,
+      discount: 0.25,
+      stockCount: 27,
+    );
+
     print("CommentsView build is called.");
 
     setCurrentScreen(widget.analytics, "Comments View", "commentsView");
@@ -31,11 +51,60 @@ class _CommentsViewState extends State<CommentsView> {
         backgroundColor: AppColors.appBarBackgroundColor,
         elevation: Dimen.appBarElevation,
         title: Text(
-          "My Comments",
+          "My Reviews",
           style: kAppBarTitleTextStyle,
         ),
         centerTitle: true,
         iconTheme: kAppBarIconStyle,
+      ),
+      body: Padding(
+        padding: Dimen.regularPadding,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: commentCount == 0 ? MainAxisAlignment.spaceBetween : MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              commentCount == 0
+                  ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Text(
+                    "No Reviews!",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 22,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: Dimen.sizedBox_15,
+                  ),
+                  Text(
+                    "You can give reviews for your products in previous orders.",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              )
+                  : SingleChildScrollView(
+                child: Wrap(
+                  children: [
+                    const Divider(thickness: Dimen.divider_2,),
+                    CommentsTile(product: dummyItem),
+                    CommentsTile(product: dummyItem),
+                    CommentsTile(product: dummyItem),
+                    CommentsTile(product: dummyItem),
+                    CommentsTile(product: dummyItem),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: NavigationBar(index: 7,),
     );
---
+++ lib/ui/comments_tile.dart
@@ -0,0 +1,171 @@
+import 'dart:ui';
+
+import 'package:cs310_footwear_project/components/footwear_item.dart';
+import 'package:cs310_footwear_project/utils/dimension.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class  CommentsTile extends StatelessWidget {
+
+  final FootWearItem product;
+
+  const CommentsTile({
+    required this.product,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: Dimen.appBarElevation,
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Image.network(
+                    product.imageUrl,
+                    width: MediaQuery.of(context).size.width /5,
+                    height: MediaQuery.of(context).size.width /5,
+                  ),
+                ],
+              ),
+              const SizedBox(width: Dimen.sizedBox_15,),
+              Column(
+                //crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      const Text(
+                        "Product Name: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        product.brandName,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: Dimen.sizedBox_5,),
+                  Row(
+                    children: [
+                      const Text(
+                        "Brand Name: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        product.brandName,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: Dimen.sizedBox_5,),
+                  Row(
+                    children: [
+                      const Text(
+                        "Seller: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        product.sellerName,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: Dimen.sizedBox_5,),
+                  Row(
+                    children: [
+                      const Text(
+                        "Your Rating: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      RatingBar.builder(
+                        initialRating: product.rating,
+                        minRating: 0,
+                        direction: Axis.horizontal,
+                        allowHalfRating: true,
+                        itemCount: 5,
+                        itemSize: 20,
+                        itemBuilder: (context, _) => const Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                          size: 5,
+                        ),
+                        onRatingUpdate: (rating) {
+                          print(rating);
+                        },
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: Dimen.sizedBox_5,),
+                  Row(
+                    children: const [
+                      Text(
+                        "Review Status: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        "Pending",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(width: Dimen.sizedBox_15,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Text("02/11/2021")
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: Dimen.sizedBox_5,),
+          Row(
+            children: const [
+              Text(
+                "Your Comment: ",
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  "jsdflsfkjbflsjkbsnflkjbsfljsgljsblgjsbblfjbslfjbslbjfslfdbslbjlsdfjbsflbjhvabflkjfbvskjbfskjfbsk",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                  ),
+                  //textAlign: TextAlign.justify,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+              ),
+            ],
+          ),
+          const Divider(thickness: Dimen.divider_2,),
+        ],
+      ),
+    );
+  }
+}
